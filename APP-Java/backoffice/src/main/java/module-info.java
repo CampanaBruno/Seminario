feb853546d6adc395a711b4617ba028babac9351
Bruno Campana <127866662+CampanaBruno@users.noreply.github.com>
2024-11-14 00:32:40 -0300

    Delete APP-Java/backoffice directory

--- APP-Java/backoffice/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.ford {
-    requires transitive javafx.controls;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.ford to javafx.fxml;
-    exports com.ford;
-}
